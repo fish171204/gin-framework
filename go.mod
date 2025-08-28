2516b0b394ae5370227519bc7077237e5c1c943c
Nguyễn Đăng Khoa <145481912+fish171204@users.noreply.github.com>
2025-08-28 21:52:40 +0700

    test lesson01

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/fish171204/gin-framework
+
+go 1.25.0
---
+++ lesson01_package_http.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"log"
+)
+
+func main() {
+	log.Println("Hello, World!")
+	fmt.Println("Hello, World!")
+}
