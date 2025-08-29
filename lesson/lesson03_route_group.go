92e1e08add8371e1093b3fb4607402c614408c1a
Nguyễn Đăng Khoa <145481912+fish171204@users.noreply.github.com>
2025-08-30 01:19:36 +0700

    refactor code : group routes into nested structure

--- lesson/lesson03_route_group.go
+++ lesson/lesson03_route_group.go
@@ -11,29 +11,39 @@ func Lesson03RouteGroup() {
 
 	v1 := r.Group("/api/v1/")
 	{
-		userV1Handler := v1handler.NewUserHandler()
-		v1.GET("/users", userV1Handler.GetUsersV1)
-		v1.GET("/users/:id", userV1Handler.GetUsersByIdV1)
-		v1.POST("/users", userV1Handler.PostUsersV1)
-		v1.PUT("/users/:id", userV1Handler.PutUsersV1)
-		v1.DELETE("/users/:id", userV1Handler.DeleteUsersV1)
+		user := v1.Group("/users")
+		{
+			userHandlerV1 := v1handler.NewUserHandler()
+			user.GET("", userHandlerV1.GetUsersV1)
+			user.GET("/:id", userHandlerV1.GetUsersByIdV1)
+			user.POST("", userHandlerV1.PostUsersV1)
+			user.PUT("/:id", userHandlerV1.PutUsersV1)
+			user.DELETE("/:id", userHandlerV1.DeleteUsersV1)
+		}
 
-		productV1Handler := v1handler.NewProductHandler()
-		v1.GET("/products", productV1Handler.GetProductV1)
-		v1.GET("/products/:id", productV1Handler.GetProductByIdV1)
-		v1.POST("/products", productV1Handler.PostProductV1)
-		v1.PUT("/products/:id", productV1Handler.PutProductV1)
-		v1.DELETE("/products/:id", productV1Handler.DeleteProductV1)
+		product := v1.Group("/products")
+		{
+			productHandlerV1 := v1handler.NewProductHandler()
+			product.GET("", productHandlerV1.GetProductV1)
+			product.GET("/:id", productHandlerV1.GetProductByIdV1)
+			product.POST("", productHandlerV1.PostProductV1)
+			product.PUT("/:id", productHandlerV1.PutProductV1)
+			product.DELETE("/:id", productHandlerV1.DeleteProductV1)
+		}
 	}
 
 	v2 := r.Group("/api/v2")
 	{
-		userV2Handler := v2handler.NewUserHandler()
-		v2.GET("/users", userV2Handler.GetUsersV2)
-		v2.GET("/users/:id", userV2Handler.GetUsersByIdV2)
-		v2.POST("/users", userV2Handler.PostUsersV2)
-		v2.PUT("/users/:id", userV2Handler.PutUsersV2)
-		v2.DELETE("/users/:id", userV2Handler.DeleteUsersV2)
+		user := v2.Group("/users")
+		{
+			userHandlerV2 := v2handler.NewUserHandler()
+			user.GET("", userHandlerV2.GetUsersV2)
+			user.GET("/:id", userHandlerV2.GetUsersByIdV2)
+			user.POST("", userHandlerV2.PostUsersV2)
+			user.PUT("/:id", userHandlerV2.PutUsersV2)
+			user.DELETE("/:id", userHandlerV2.DeleteUsersV2)
+		}
+
 	}
 
 	r.Run(":8080")
