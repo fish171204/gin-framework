2dbd263da13b80a8064940d619b5d208cfb31548
Nguyễn Đăng Khoa <145481912+fish171204@users.noreply.github.com>
2025-08-30 00:55:18 +0700

    add version 1 and version 2 to API group

--- lesson/lesson03_route_group.go
+++ lesson/lesson03_route_group.go
@@ -9,26 +9,32 @@ import (
 func Lesson03RouteGroup() {
 	r := gin.Default()
 
-	userV1Handler := v1handler.NewUserHandler()
-	r.GET("/api/v1/users", userV1Handler.GetUsersV1)
-	r.GET("/api/v1/users/:id", userV1Handler.GetUsersByIdV1)
-	r.POST("/api/v1/users", userV1Handler.PostUsersV1)
-	r.PUT("/api/v1/users/:id", userV1Handler.PutUsersV1)
-	r.DELETE("api/v1/users/:id", userV1Handler.DeleteUsersV1)
+	v1 := r.Group("/api/v1/")
+	{
+		userV1Handler := v1handler.NewUserHandler()
+		v1.GET("/users", userV1Handler.GetUsersV1)
+		v1.GET("/users/:id", userV1Handler.GetUsersByIdV1)
+		v1.POST("/users", userV1Handler.PostUsersV1)
+		v1.PUT("/users/:id", userV1Handler.PutUsersV1)
+		v1.DELETE("/users/:id", userV1Handler.DeleteUsersV1)
 
-	productV1Handler := v1handler.NewProductHandler()
-	r.GET("/api/v1/products", productV1Handler.GetProductV1)
-	r.GET("/api/v1/products/:id", productV1Handler.GetProductByIdV1)
-	r.POST("/api/v1/products", productV1Handler.PostProductV1)
-	r.PUT("/api/v1/products/:id", productV1Handler.PutProductV1)
-	r.DELETE("api/v1/products/:id", productV1Handler.DeleteProductV1)
+		productV1Handler := v1handler.NewProductHandler()
+		v1.GET("/products", productV1Handler.GetProductV1)
+		v1.GET("/products/:id", productV1Handler.GetProductByIdV1)
+		v1.POST("/products", productV1Handler.PostProductV1)
+		v1.PUT("/products/:id", productV1Handler.PutProductV1)
+		v1.DELETE("/products/:id", productV1Handler.DeleteProductV1)
+	}
 
-	userV2Handler := v2handler.NewUserHandler()
-	r.GET("/api/v2/users", userV2Handler.GetUsersV2)
-	r.GET("/api/v2/users/:id", userV2Handler.GetUsersByIdV2)
-	r.POST("/api/v2/users", userV2Handler.PostUsersV2)
-	r.PUT("/api/v2/users/:id", userV2Handler.PutUsersV2)
-	r.DELETE("api/v2/users/:id", userV2Handler.DeleteUsersV2)
+	v2 := r.Group("/api/v2")
+	{
+		userV2Handler := v2handler.NewUserHandler()
+		v2.GET("/users", userV2Handler.GetUsersV2)
+		v2.GET("/users/:id", userV2Handler.GetUsersByIdV2)
+		v2.POST("/users", userV2Handler.PostUsersV2)
+		v2.PUT("/users/:id", userV2Handler.PutUsersV2)
+		v2.DELETE("/users/:id", userV2Handler.DeleteUsersV2)
+	}
 
 	r.Run(":8080")
 }
