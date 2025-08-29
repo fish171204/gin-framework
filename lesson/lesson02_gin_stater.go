3899a671a103f41e082fae973c4b7170758cd918
Nguyễn Đăng Khoa <145481912+fish171204@users.noreply.github.com>
2025-08-29 23:48:39 +0700

    end point & path param & query param

--- lesson/lesson02_gin_stater.go
+++ lesson/lesson02_gin_stater.go
@@ -10,5 +10,38 @@ func Lesson02GinStarter() {
 		ctx.JSON(200, gin.H{"message": "Nguyen Dang Khoa"})
 	})
 
+	r.GET("/users/:user_id", func(ctx *gin.Context) {
+		ctx.JSON(200, gin.H{"data": "Danh sach user"})
+	})
+
+	r.GET("/user/:user_id", func(ctx *gin.Context) {
+		user_id := ctx.Param("user_id")
+		ctx.JSON(200, gin.H{
+			"data":    "Thong tin user",
+			"user_id": user_id,
+		})
+	})
+
+	r.GET("/products/detail/:product_name", func(ctx *gin.Context) {
+		product_name := ctx.Param("product_name")
+		page := ctx.Query("page")
+		ctx.JSON(200, gin.H{
+			"data:":        "Thong tin san pham",
+			"product_name": product_name,
+			"page":         page,
+		})
+	})
+
+	r.GET("/product/:product_name", func(ctx *gin.Context) {
+		product_name := ctx.Param("product_name")
+		price := ctx.Query("price")
+		color := ctx.Query("color")
+		ctx.JSON(200, gin.H{
+			"data":          "Thong tin san pham",
+			"product_name":  product_name,
+			"product_price": price,
+			"color":         color,
+		})
+	})
 	r.Run(":8080")
 }
