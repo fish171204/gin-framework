b297f3e2257a1b8a7296c8c88429f9257131e2a7
Nguyễn Đăng Khoa <145481912+fish171204@users.noreply.github.com>
2025-09-01 23:59:16 +0700

    use ShouldBind() for text fields in form-data

--- internal/api/v1/handler/news.go
+++ internal/api/v1/handler/news.go
@@ -3,6 +3,7 @@ package v1handler
 import (
 	"net/http"
 
+	"github.com/fish171204/gin-framework/utils"
 	"github.com/gin-gonic/gin"
 )
 
@@ -34,17 +35,17 @@ func (u *NewsHandler) GetNewsV1(ctx *gin.Context) {
 	}
 }
 
-// func (u *NewsHandler) PostNewsV1(ctx *gin.Context) {
-// 	var params PostNewsV1Param
-// 	if err := ctx.ShouldBind(&params); err != nil {
-// 		ctx.JSON(http.StatusBadRequest, utils.HandleValidationErrors(err))
-// 		return
-// 	}
+func (u *NewsHandler) PostNewsV1(ctx *gin.Context) {
+	var params PostNewsV1Param
+	if err := ctx.ShouldBind(&params); err != nil {
+		ctx.JSON(http.StatusBadRequest, utils.HandleValidationErrors(err))
+		return
+	}
 
-// 	ctx.JSON(http.StatusOK, gin.H{
-// 		"message": "Post category (V1)",
-// 		"title":   params.Title,
-// 		"status":  params.Status,
-// 	})
+	ctx.JSON(http.StatusOK, gin.H{
+		"message": "Post category (V1)",
+		"title":   params.Title,
+		"status":  params.Status,
+	})
 
-// }
+}
