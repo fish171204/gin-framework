bd3dc0f555557b3ab32c7926f92e1c5aa3d9a446
Nguyễn Đăng Khoa <145481912+fish171204@users.noreply.github.com>
2025-09-03 22:45:06 +0700

    complete getRateLimiter function

--- middleware/rate_limiting.go
+++ middleware/rate_limiting.go
@@ -2,8 +2,19 @@ package middleware
 
 import (
 	"log"
+	"time"
 
 	"github.com/gin-gonic/gin"
+	"golang.org/x/time/rate"
+)
+
+type Client struct {
+	limiter  *rate.Limiter
+	lassSeen time.Time
+}
+
+var (
+	clients = make(map[string]Client)
 )
 
 func getClientIP(ctx *gin.Context) string {
@@ -15,9 +26,26 @@ func getClientIP(ctx *gin.Context) string {
 	return ip
 }
 
+func getRateLimiter(ip string) *rate.Limiter {
+	// mu.Lock()
+	// defer mu.Unlock()
+	client, exists := clients[ip]
+	// IP does not exist → create new
+	if !exists {
+		limiter := rate.NewLimiter(5, 10) // 5 request/s , brust : 10 (max)
+		newClient := &Client{limiter, time.Now()}
+		clients[ip] = *newClient
+
+		return limiter
+	}
+
+	client.lassSeen = time.Now()
+	return client.limiter
+}
+
 func RateLimitingMiddleware() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
 		ip := getClientIP(ctx)
-		log.Println(ip)
+		log.Default()
 	}
 }
