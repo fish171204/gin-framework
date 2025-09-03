2a54d80b8c168f8dca4e455ba04c6664bed9f524
Nguyễn Đăng Khoa <145481912+fish171204@users.noreply.github.com>
2025-09-03 23:56:15 +0700

    initialize logger with Zerolog

--- middleware/logger.go
+++ middleware/logger.go
@@ -5,6 +5,7 @@ import (
 	"path/filepath"
 
 	"github.com/gin-gonic/gin"
+	"github.com/rs/zerolog"
 )
 
 func LoggerMiddleware() gin.HandlerFunc {
@@ -13,6 +14,13 @@ func LoggerMiddleware() gin.HandlerFunc {
 	if err := os.MkdirAll(filepath.Dir(logPath), os.ModePerm); err != nil {
 		panic(err)
 	}
+
+	logFile, err := os.OpenFile(logPath, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0644)
+	if err != nil {
+		panic(err)
+	}
+
+	logger := zerolog.New(logFile).With().Timestamp().Logger()
 
 	return func(ctx *gin.Context) {
 
