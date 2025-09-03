062d77f4118c050c398579e8579cd69f3f3452d4
Nguyễn Đăng Khoa <145481912+fish171204@users.noreply.github.com>
2025-09-04 00:00:33 +0700

    log request details (method, path, query, client IP,...)

--- middleware/logger.go
+++ middleware/logger.go
@@ -3,6 +3,7 @@ package middleware
 import (
 	"os"
 	"path/filepath"
+	"time"
 
 	"github.com/gin-gonic/gin"
 	"github.com/rs/zerolog"
@@ -23,6 +24,29 @@ func LoggerMiddleware() gin.HandlerFunc {
 	logger := zerolog.New(logFile).With().Timestamp().Logger()
 
 	return func(ctx *gin.Context) {
+		statt := time.Now()
+
+		ctx.Next()
+
+		duration := time.Since(statt)
+
+		logEvent := logger.Info()
+
+		logEvent.
+			Str("method", ctx.Request.Method).
+			Str("path", ctx.Request.URL.Path).
+			Str("query", ctx.Request.URL.RawPath).
+			Str("client_ip", ctx.ClientIP()).
+			Str("user_agent", ctx.Request.UserAgent()). // FireFox, Google, Safari...
+			Str("referer", ctx.Request.Referer()).      // Zalo, Fb -> my API
+			Str("protocal", ctx.Request.Proto).         // http, https
+			Str("host", ctx.Request.Host).
+			Str("remote_addr", ctx.Request.RemoteAddr). // Proxy add: 1.1.1.
+			Str("request_uri", ctx.Request.RequestURI).
+			Int64("content_length", ctx.Request.ContentLength).
+			Interface("headers", ctx.Request.Header).
+			Int("status_code", ctx.Writer.Status()).
+			Int64("duration_ms", duration.Microseconds())
 
 	}
 }
