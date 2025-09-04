fff70d61dabbb4cd1426913afc1f4883e79aafd5
Nguyễn Đăng Khoa <145481912+fish171204@users.noreply.github.com>
2025-09-04 17:37:18 +0700

    refactor code

--- middleware/logger.go
+++ middleware/logger.go
@@ -36,7 +36,7 @@ func LoggerMiddleware() gin.HandlerFunc {
 
 		// Content-Type: multipart/form-data
 		if strings.HasPrefix(contentType, "multipart/form-data") {
-			if err := ctx.Request.ParseMultipartForm(32 << 20); err != nil && ctx.Request.MultipartForm != nil {
+			if err := ctx.Request.ParseMultipartForm(32 << 20); err == nil && ctx.Request.MultipartForm != nil {
 				// for value
 				for key, vals := range ctx.Request.MultipartForm.Value {
 					if len(vals) == 1 {
